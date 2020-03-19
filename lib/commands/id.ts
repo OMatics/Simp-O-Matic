bcc5d333138720e3aea9f43cd9be231a87c5abc6
Demonstrandum <moi@knutsen.co>
2020-03-19 17:10:50 +0000

    FUCK didnt save the file...

--- lib/commands/id.ts
+++ lib/commands/id.ts
@@ -3,14 +3,8 @@ import { Message } from 'discord.js';
 export default home_scope => {
 	const { message } : { message : Message } = home_scope;
 	const rep = [];
-<<<<<<< HEAD
 	['channel', 'user', 'role'].forEach(n =>
 		message.mentions[`${n}s`].forEach(o => rep.push(`${n} id: \`${o.id}\``)));
-=======
-	message.mentions.channels.each(o => rep.push('channel id: `' + o.id + '`'))
-	message.mentions.users.each(o => rep.push('user id: `' + o.id + '`'))
-	message.mentions.roles.each(o => rep.push('role id: `' + o.id + '`'))
->>>>>>> 2fb48d88264b67abc4f73d99871d1a4ea78290e5
 	// Joining an empty array yields an empty string which is false
 	const reply = rep.join(', ') || `User ID: \`${message.author.id}\`
 		Author: ${message.author}
