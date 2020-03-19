457b8991418e540dab1e7adf90065917cb898dc8
danyisill <danyisill@users.noreply.github.com>
2020-03-19 16:14:40 +0300

    Create cowsay.ts and figlet.ts

---
+++ lib/commands/figlet.ts
@@ -0,0 +1,5 @@
+import { execFileSync } from 'child_process'
+export default home_scope => {
+	const { message, args } = home_scope
+	message.answer('``` \n' + execFileSync('/bin/figlet', args) + '```') // this is safe because no shell is spawned
+}
