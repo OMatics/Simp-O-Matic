96428e6467e17e7fb6498118411bc87af62d75c9
Daniel <23189912+danyisill@users.noreply.github.com>
2020-03-19 19:52:49 +0300

    Update pp.ts

--- lib/commands/pp.ts
+++ lib/commands/pp.ts
@@ -5,11 +5,7 @@ export default home_scope => {
 	try {
 		user = message.mentions.users.first().id;
 	} finally {
-		const shaft = '='.repeat(
-			CONFIG.pp_sizes[user]
-				? CONFIG.pp_sizes[user]
-				: CONFIG.pp_sizes[user] = Math.ceil(Math.random() * 16));
-
+		const shaft = '='.repeat(CONFIG.pp_sizes[user] || CONFIG.pp_sizes[user] = Math.ceil(Math.random() * 16));
 		message.answer(`8${shaft}>`);
 	}
 };
