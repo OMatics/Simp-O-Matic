af8f828db3eb1c7c3001374ef8b836ad04c1c9de
Daniel <23189912+danyisill@users.noreply.github.com>
2020-03-19 19:50:06 +0300

    Update id.ts

--- lib/commands/id.ts
+++ lib/commands/id.ts
@@ -1,7 +1,7 @@
 export default home_scope => {
 	const { message } = home_scope;
 	const rep = [];
-	['channel', 'user', 'role'].each(n =>
+	['channel', 'user', 'role'].forEach(n =>
 		message[n + 's'].each(o => rep.push(n + ' id: `' + o.id + '`')));
 	// Joining an empty array yields an empty string which is false
 	const reply = rep.join(', ') || `User ID: \`${message.author.id}\`
