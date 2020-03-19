aaf7612866b73e837d4be9cd4d6821246e6a50c4
danyisill <danyisill@users.noreply.github.com>
2020-03-19 17:59:35 +0300

    smol fx

--- lib/commands/cowsay.ts
+++ lib/commands/cowsay.ts
@@ -1,8 +1,8 @@
-import { execFileSync as exec } from 'child_process'
+import { execFileSync as exec_file_sync } from 'child_process'
 export default home_scope => {
 	const { message, args } = home_scope;
 	// This is safe because no shell is spawned:
-	message.answer(exec('./node_modules/.bin/cowsay', args, {
+	message.answer(exec_file_sync('./node_modules/.bin/cowsay', args, {
 		encoding: 'utf8',
 		cwd: process.cwd()
 	}).format('```'));
