8b64e973d035cb6a2bcdcdd9dc7a69610712c24c
danyisill <danyisill@users.noreply.github.com>
2020-03-19 16:20:42 +0300

    return string

--- lib/commands/figlet.ts
+++ lib/commands/figlet.ts
@@ -1,5 +1,5 @@
 import { execFileSync } from 'child_process'
 export default home_scope => {
 	const { message, args } = home_scope
-	message.answer('``` \n' + execFileSync('/bin/figlet', args) + '```') // this is safe because no shell is spawned
+	message.answer('``` \n' + execFileSync('/bin/figlet', args) + '```', {encoding: 'utf8'}) // this is safe because no shell is spawned
 }
