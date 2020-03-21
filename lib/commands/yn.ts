ee6daa0987d018fe763b4788faa22a071def2a36
Fredrik <moi@knutsen.co>
2020-03-21 15:29:57 +0000

    Bind function back to its original object.

--- lib/commands/yn.ts
+++ lib/commands/yn.ts
@@ -1,4 +1,4 @@
 export default home_scope => {
 	const { message } = home_scope
-	Promise.all(['✅', '❎'].map(message.react)).then(console.log).catch(console.log)
+	Promise.all(['✅', '❎'].map(message.react.bind(message))).then(console.log).catch(console.log)
 };
