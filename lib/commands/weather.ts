56446b6fe9a82d02c1a5486da28c84a58aa613fc
danyisill <danyisill@users.noreply.github.com>
2020-03-24 01:51:44 +0300

    Fix Weather

--- lib/commands/weather.ts
+++ lib/commands/weather.ts
@@ -51,25 +51,25 @@ export default (home_scope: HomeScope) => {
 	.catch(error)
 	.then(res => res.json())
 	.then(c => {
-		let latlon = c.response.featureMember[0].GeoObject.Point.pos.split(' ')
+		let latlon = c.response.GeoObjectCollection.featureMember[0].GeoObject.Point.pos.split(' ').reverse()
 		fetch(`${WEATHER_URL}/${key}/${latlon}?exclude=minutely,hourly,alerts,flags&units=si`)
 			.catch(error)
 			.then(res => res.json())
 			.then(d => {
-				const date = new Date(d.currently.time);
+				const date = new Date(d.currently.time * 1000);
+				const dateString = date.toLocaleTimeString('en-GB', {hour: '2-digit', minute: '2-digit', timeZone: d.timezone});
 				const embed = d && d.currently
 					? new MessageEmbed()
 						.setTitle(`${d.currently.temperature}°C (feels like ${d.currently.apparentTemperature}°C)`)
-						.setAuthor(`${ICONS[d.currently.icon]} ${date.getHours()}:${date.getMinutes()} ${c.response.featureMember[0].GeoObject.name}`)
-						.setDescription(MOON_PHASES[Math.round(d.daily.data[0].moonPhase * 7)] + ' ' +d.currently.summary)
+						.setAuthor(`${ICONS[d.currently.icon]} ${dateString} ${c.response.GeoObjectCollection.featureMember[0].GeoObject.name}, ${c.response.GeoObjectCollection.featureMember[0].GeoObject.description}`)
+						.setDescription(MOON_PHASES[Math.round(d.daily.data[0].moonPhase * 7)] + ' ' +d.currently.summary + '.')
 						.addFields(
 							{ name: 'daytime',   value: d.daily.data[0].temperatureHigh + '°C', inline: true },
 							{ name: 'nighttime', value: d.daily.data[0].temperatureLow + '°C', inline: true },
 							{ name: 'humidity',  value: d.currently.humidity + '%', inline: true},
 							{ name: 'wind', value: `${DIRECTIONS[Math.round(d.currently.windBearing / 45) % 8]} ${d.currently.windSpeed}㎧`, inline: true })
 						.setFooter('Powered by Dark Sky', 'https://darksky.net/images/darkskylogo.png')
-					: new MessageEmbed().setTitle(`Cannot get weather information from ${location}.`)
-
+					: new MessageEmbed().setTitle(`Cannot get weather information from ${location}.`);
 				message.channel.send(embed);
 			}).catch(error);
 		})
