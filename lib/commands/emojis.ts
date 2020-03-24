e58f7d2df41fb03588cb8ecdd77af06f3a85cca0
Bruno <b-coimbra@hotmail.com>
2020-03-24 16:45:41 -0300

    Update emojis.ts

--- lib/commands/emojis.ts
+++ lib/commands/emojis.ts
@@ -1,6 +1,6 @@
 export default (home_scope: HomeScope) => {
 	const { message } = home_scope;
 	message.channel.send(
-		message.guild.emojis.cache.map(x => x.name.emojify).join(' ')
+		message.guild.emojis.cache.map(x => x.name.emojify()).join(' ')
 	);
 };
