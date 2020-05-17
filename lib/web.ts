031e569f4d0ac71c221153982670e86796c06b16
Demonstrandum <moi@knutsen.co>
2020-05-17 18:46:11 +0100

    ENFATTEN the web-server (added support for more mime-types), added favicons, added SVG logo.

--- lib/web.ts
+++ lib/web.ts
@@ -2,6 +2,16 @@ import http from 'http';
 import { readFileSync as read_file } from 'fs';
 import process from 'process';
 
+const templater = (doc, vars): string => {
+	let new_doc = doc.toString();
+	for (const key of Object.keys(vars)) {
+		const match = new RegExp(`{{\s*${key}\s*}}`, 'g');
+		new_doc = new_doc
+			.replace(match, vars[key]);
+	}
+	return new_doc;
+};
+
 const PORT = Number(process.env.PORT) || 8080;
 
 export default (GLOBAL_CONFIG : Types.GlobalConfig, handle_post) => {
@@ -23,8 +33,58 @@ export default (GLOBAL_CONFIG : Types.GlobalConfig, handle_post) => {
 			});
 			return;
 		}
+		
+		const url = req.url;
+		const relative = `./web/${url}`;
+		if (url.endsWith('.png')) {
+			try {
+				const content = read_file(relative);
+				res.writeHead(200, {
+					'Content-Type': 'image/png'
+				});
+				res.write(content);
+				res.end();
+			} catch {}
+		} else if (url.endsWith('.svg')) {
+			try {
+				const content = read_file(relative);
+				res.writeHead(200, {
+					'Content-Head': 'application/svg+xml'
+				});
+				res.write(content);
+				res.end();
+			} catch {}
+		} else if (url.endsWith('.xml')) {
+			try {
+				const content = read_file(relative);
+				res.writeHead(200, {
+					'Content-Type': ' application/xml'
+				});
+				res.write(content);
+				res.end();
+			} catch {}
+		} else if (url.endsWith('.ico')) {
+			try {
+				const content = read_file(relative);
+				res.writeHead(200, {
+					'Content-Type': 'image/x-icon'
+				});
+				res.write(content);
+				res.end();
+			} catch {}
+		} else if (url.endsWith('.webmanifest')) {
+			try {
+				const content = read_file(relative);
+				res.writeHead(200, {
+					'Content-Type': 'application/manifest+json'
+				});
+				res.write(content);
+				res.end();
+			} catch {}
+		}
 
-		switch (req.url) {
+		const favicons = read_file('./web/favicons.html');
+		switch (url) {
 			case '/':
 				res.writeHead(200, {
 					'Content-Type': 'text/html'
@@ -35,23 +95,29 @@ export default (GLOBAL_CONFIG : Types.GlobalConfig, handle_post) => {
 					.length
 					.toString();
 
-				const index_contents = read_file('./web/index.html')
-					.toString()
-					.replace(/{{\s*GUILD_COUNT\s*}}/g, guild_count);
+				const index_contents = templater(
+					read_file('./web/index.html'),
+					{
+						'GUILD_COUNT': guild_count,
+						'FAVICONS': favicons
+					});
 				res.write(index_contents);
+				res.end();
+				break;
+			case '/config.json':
+				res.writeHead(200, {
+					'Content-Type': 'application/json'
+				});
+				res.write(JSON.stringify(GLOBAL_CONFIG, null, 4));
 				res.end();
 				break;
 			default:
 				res.writeHead(404, {
 					'Content-Type': 'text/html'
 				});
-				res.write(`<!DOCTYPE html>
-			<html>
-			<body>
-				<h1>404 - Not found.</h1>
-			</body>
-			</html>
-		`);
+				res.write(templater(read_file('./web/404.html'), {
+					'FAVICONS': favicons
+				}));
 				res.end();
 		}
 	};
