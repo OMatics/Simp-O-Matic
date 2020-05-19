63191c7b473a50107aef1627aacfaea1d738b034
Daniel <23189912+danyisill@users.noreply.github.com>
2020-05-20 00:31:37 +0300

    Update abbreviate.ts

--- lib/commands/abbreviate.ts
+++ lib/commands/abbreviate.ts
@@ -1,4 +1,4 @@
 export default async (home_scope: HomeScope) => {
 	const { message, args } = home_scope;
-	message.channel.send(args.map(w => [...w].find(c => isNaN(c)?c.toLowerCase() != c.toUpperCase():c) || '').join('').toUpperCase() + ` (${args})`)
+	message.channel.send(args.map(w => [...w].find(c => isNaN(c as any)?c.toLowerCase() != c.toUpperCase():c) || '').join('').toUpperCase() + ` (${args})`)
 }
