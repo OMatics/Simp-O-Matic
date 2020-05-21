706c67f586c87d95c8f1031846ac1fc6c43c0b0e
Demonstrandum <moi@knutsen.co>
2020-05-21 22:32:47 +0100

    Fix very dumb regex mistake.

--- lib/commands/choose.ts
+++ lib/commands/choose.ts
@@ -1,7 +1,7 @@
 export default (home_scope: HomeScope) => {
 	const { message, args } = home_scope;
 	const a = args.length
-		? args.join(' ').split(/\s*(?:,|or)\s*/)
-		: ['I need a list.'];
+		? args.join(' ').split(/\s*(?:,|\bor\b)\s*/gi)
+		: [ 'I need a list.' ];
 	message.answer(a[Math.floor(Math.random() * a.length)]);
 };
