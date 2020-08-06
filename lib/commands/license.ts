52951ebaa34482895182b3543d1434e333f67c53
Demonstrandum <moi@knutsen.co>
2020-08-06 17:00:28 +0100

    Delete accidental inclusion of old code

--- lib/commands/license.ts
+++ lib/commands/license.ts
@@ -1,6 +1,5 @@
 export default (home_scope: HomeScope) => {
 	const { message } = home_scope;
-			.catch(console.warn);
 	message.channel.send(
 		"**GNU Affero GPLv3 (`AGPL-3.0`), _Free_ as in Freedom.**");
 	message.channel.send("<:AGPL:740958423008411678>");
